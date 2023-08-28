e9bb82c93d36f76ba2de5df85698a9fe7774086a
jeremia27 <jeremiasibarani51@gmail.com>
2023-08-28 19:06:47 +0700

    fixing anchor conflic

--- src/components/Pages/NavBot/NavBoot.js
+++ src/components/Pages/NavBot/NavBoot.js
@@ -11,16 +11,16 @@ function NavBot() {
     return (
         <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
             <Container >
-                        <Navbar.Brand href="#home">My Portfolio</Navbar.Brand>
-                        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                        <Navbar.Collapse className='nav-collapse' id="justify-content-end"  >
-                            <Nav className='ms-auto'>
-                                <Nav.Link href="#about" className="mb-2 mb-lg-0" style={{ fontSize: 20 }} onClick={()=> scrollToSection("about")}>About</Nav.Link>
-                                <Nav.Link href="#experience" className="mb-2 mb-lg-0" style={{ fontSize: 20 }} onClick={()=> scrollToSection("experience")}>Experience</Nav.Link>
-                                <Nav.Link href="#project" className="mb-2 mb-lg-0" style={{ fontSize: 20 }} onClick={()=> scrollToSection("project")}>Project</Nav.Link>
-                                <Nav.Link href="#contact" className="mb-2 mb-lg-0" style={{ fontSize: 20 }} onClick={()=> scrollToSection("contact")}>Contact</Nav.Link>
-                            </Nav>
-                        </Navbar.Collapse>
+                <Navbar.Brand href="#home">My Portfolio</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse className='nav-collapse' id="justify-content-end"  >
+                    <Nav className='ms-auto'>
+                        <Nav.Link href="#about" className="mb-2 mb-lg-0" style={{ fontSize: 20 }} onClick={() => scrollToSection("about")}>About</Nav.Link>
+                        <Nav.Link href="#experience" className="mb-2 mb-lg-0" style={{ fontSize: 20 }} onClick={() => scrollToSection("experience")}>Experience</Nav.Link>
+                        <Nav.Link href="#project" className="mb-2 mb-lg-0" style={{ fontSize: 20 }} onClick={() => scrollToSection("project")}>Project</Nav.Link>
+                        <Nav.Link href="#contact" className="mb-2 mb-lg-0" style={{ fontSize: 20 }} onClick={() => scrollToSection("contact")}>Contact</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
             </Container>
         </Navbar>
     );
