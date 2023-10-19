8c3b774476f9a0d663495f03e80a0b19848a45a7
jeremia27 <jeremiasibarani51@gmail.com>
2023-10-19 07:35:29 +0700

    update Text

--- src/components/Pages/Home/Home.js
+++ src/components/Pages/Home/Home.js
@@ -1,16 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Image from 'react-bootstrap/Image';
 import Profile from '../../Assets/myphoto2.png';
 import './Home.css';
 
 export default function Home() {
+
+    const text = "Frontend Developer React.js";
+    const [displayText, setDisplayText] = useState('');
+
+    useEffect(() => {
+        let currentIndex = 0;
+        const timer = setInterval(() => {
+          if (currentIndex <= text.length) {
+            setDisplayText(text.slice(0, currentIndex));
+            currentIndex++;
+          } else {
+            clearInterval(timer);
+          }
+        }, 100); // Sesuaikan dengan kecepatan yang Anda inginkan
+    
+        return () => {
+          clearInterval(timer);
+        };
+      }, []);
     return (
         <div className='about-container'>
-            
+
             <div className='content-profile'>
                 <p className='section-txt-p1'>Hello, I'm</p>
                 <h1 className='title-home'>Jeremia</h1>
-                <p className='section-txt-p2'>Frontend Developer React.js</p>
+                <p className='section-txt-p2 typing-text'>{displayText}</p>
                 {/* <div className='btn-container'>
                     <button className='btn btn-color-1'>
                         Download CV
